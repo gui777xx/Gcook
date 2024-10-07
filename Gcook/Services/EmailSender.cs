bf4bbde9ea2118bfa34eff6b6fb1627f146f4357
gui777x <guizin.clementino@gmail.com>
2024-10-07 09:53:36 -0300

    feito porem com erros

--- Gcook/Services/EmailSender.cs
+++ Gcook/Services/EmailSender.cs
@@ -3,7 +3,7 @@ using System.Net.Mail;
 
 namespace GCook.Services;
 
-public class EmailSender : IEmailSender
+public class EmailSender : IEmailSender 
 {
   public async Task SendEmailAsync(string emailAddress, string subject, string htmlMessage) 
   {
